5057495023ba634c84eeb9ed802bb0659b42a529
Sinan Rassam <sinan.rassam@outlook.com>
2020-08-26 17:29:52 +1200

    Fixed load issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 var delay = 500;
+$( document ).ready(function() {
+    window.setTimeout(function () {
+        $('.container').toggleClass('hidden');
+        $('body').toggleClass('animate');
+    }, delay);
+});
+
 $('.special-link').on('click', function () {
     $('.card').toggleClass('flipped');
     window.setTimeout(function () {
